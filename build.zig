21f755efce288bd19c00692006d9f465ed661b9d
mkashirin <m.k4shirin@yandex.ru>
2024-09-28 17:29:02 +0300

    Remove chllng dir

--- build.zig
+++ build.zig
@@ -14,6 +14,25 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
+    // This allows for ZLS to check on the errors occuring in the build
+    // process. Be sure to add the following settings to your "zls.json" for
+    // this to work:
+    //
+    // ```json
+    // {
+    //   "enable_build_on_save": true,
+    //   "build_on_save_step": "check",
+    // }
+    // ```
+    const exe_check = b.addExecutable(.{
+        .name = "zlox",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const check_step = b.step("check", "Check if zlox compiles");
+    check_step.dependOn(&exe_check.step);
+
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,35 @@
-// This is empty for now.
+const std = @import("std");
+
+const fs = std.fs;
+const heap = std.heap;
+const mem = std.mem;
+const process = std.process;
+
+const Allocator = mem.Allocator;
+
+const config = @import("config.zig");
+
+pub fn main(args: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
+
+    if (args.len > 1) {
+        config.log(.info, config.usage_message);
+        process.exit(64);
+    } else if (args.len == 1) try runFile(args[0], allocator) else runPrompt();
+}
+
+pub fn runFile(absolute_path: []const u8, arena: Allocator) !void {
+    const file = try std.fs.openFileAbsolute(absolute_path, .{});
+    defer file.close();
+    try file.seekTo(0);
+    const contents = &try file.readToEndAlloc(arena, 8192);
+    run(contents);
+}
+
+pub fn runPrompt() void {}
+
+pub fn run(contents: []const u8) void {
+    _ = contents;
+}
